9c986124bfd0b899f2497e8a7d62ac186bc3ac1c
Filipe Marcilli <filipemar9@gmail.com>
2022-01-04 15:28:05 -0300

    .

--- modules/vm-windows/datasources.tf
+++ modules/vm-windows/datasources.tf
@@ -1,4 +1,5 @@
 data "azurerm_key_vault" "keyvault" {
-  resource_group_name = var.rg_name
+  #resource_group_name = var.rg_name
+  resource_group_name  = "${data.azurerm_resource_group.rg_all.name}" 
   name = "keyvault-${var.project_name}-${var.environment}"
 }
