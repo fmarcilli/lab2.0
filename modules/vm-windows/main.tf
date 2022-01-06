72da572085855056d7de1eb4a5fbd5604e7b4e6a
Filipe Marcilli <filipemar9@gmail.com>
2022-01-06 11:50:05 -0300

    KV add filipe.marcilli identity

--- modules/vm-windows/main.tf
+++ modules/vm-windows/main.tf
@@ -77,7 +77,7 @@ name = "${each.value}-PIP"
 # Only use for the Keyvault to store password
 # VM User Identity #
 resource "azurerm_user_assigned_identity" "vm_user_assigned_identity" {
-  name                = "Filipe Marcilli"
+  name                = "filipemar9_hotmail.com#EXT#@filipemar9hotmail.onmicrosoft.com"
   resource_group_name  = "${data.azurerm_resource_group.rg_all.name}" 
   location            = "Brazil South"
 }
