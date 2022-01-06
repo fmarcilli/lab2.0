d782a81821fd9658048c42f92c2d87443c4ca939
Filipe Marcilli <filipemar9@gmail.com>
2022-01-06 16:05:49 -0300

    .

--- modules/vm-windows/main.tf
+++ modules/vm-windows/main.tf
@@ -109,7 +109,7 @@ resource "random_password" "rdm_password" {
 
 # VM Secret Create
 resource "azurerm_key_vault_secret" "kv_secret" {
-  name         = "secretvmlab1"
+  name         = "secretvmlab10"
   value        = random_password.rdm_password.result 
   key_vault_id = data.azurerm_key_vault.keyvault.id
 }
